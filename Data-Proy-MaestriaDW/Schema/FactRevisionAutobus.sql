2a6528ed062b32d6a22ee5eaaa2e162abca653fe
Roger Diego Flores Condori <rogerdiegof@gmail.com>
2024-10-01 21:02:14 -0400

    dimensiones y fact

---
+++ Data-Proy-MaestriaDW/Schema/FactRevisionAutobus.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [FactRevisionAutobus] (
+    [idRevisionAutobusSk] INTEGER IDENTITY(0,1) NOT NULL,
+    [idAutobusSk] INTEGER,
+    [idRevisionHistorico] INTEGER,
+    [idReparacionHistorico] INTEGER,
+    [idRevisionFechaRevisionSk] INTEGER,
+    [revisionDiagnostico] NVARCHAR(max),
+    [revisionCostoRevision] DECIMAL(10,2),
+    [reparacionExisteReparacion] BIT,
+    [reparacionCodigoReparacion] NVARCHAR(20),
+    [reparacionTiempoEmpleado] INTEGER,
+    [reparacionComentarioReparacion] NVARCHAR(max),
+    [reparacionCostoReparacion] DECIMAL(10,2),
+    PRIMARY KEY ([idRevisionAutobusSk])
+)
+GO
+
+
+ALTER TABLE [FactRevisionAutobus] ADD CONSTRAINT [DimAutobus_FactRevisionAutobus] 
+    FOREIGN KEY ([idAutobusSk]) REFERENCES [DimAutobus] ([idAutobusSk])
+GO
+
+ALTER TABLE [FactRevisionAutobus] ADD CONSTRAINT [DimTiempo_FactRevisionAutobus] 
+    FOREIGN KEY ([idRevisionFechaRevisionSk]) REFERENCES [DimTiempo] ([idTiempoSk])
+GO
---
+++ Data-Proy-MaestriaDW/Schema/FactServicioViaje.sql
@@ -0,0 +1,66 @@
+﻿CREATE TABLE [FactServicioViaje] (
+    [idServicioViajeSk] INTEGER IDENTITY(0,1) NOT NULL,
+    [idAutobusSk] INTEGER,
+    [idBoletoSk] INTEGER,
+    [idConductorSk] INTEGER,
+    [idTuristaSk] INTEGER,
+    [idRutaLugarSk] INTEGER,
+    [idServicioHistorico] INTEGER,
+    [idViajeHistorico] INTEGER,
+    [idCompraHistorico] INTEGER,
+    [servicioTarifaTransporte] DECIMAL(10,2),
+    [servicioTarifaActividadTuristica] DECIMAL(10,2),
+    [servicioHoraSalida] TIME NOT NULL,
+    [servicioHoraLlegadaEstimada] TIME,
+    [servicioEsServicioDiario] BIT,
+    [servicioLunes] BIT,
+    [servicioMartes] BIT,
+    [servicioMiercoles] BIT,
+    [servicioJueves] BIT,
+    [servicioViernes] BIT,
+    [servicioSabado] BIT,
+    [servicioDomingo] BIT,
+    [servicioEsParaDiaFestivo] BIT,
+    [viajeHorasTotalesViaje] DECIMAL(10,2),
+    [viajeHoraRealLlegada] TIME,
+    [viajeHoraRealSalida] TIME,
+    [viajeKilometrosRecorridoReal] DECIMAL(10,2),
+    [idViajeFechaViajeSk] INTEGER,
+    [viajeFinalizado] BIT,
+    [compraCostoTransporte] DECIMAL(10,2) NOT NULL,
+    [compraCostoActividadTuristica] DECIMAL(10,2),
+    [compraIncluyeTransporte] BIT,
+    [compraIncluyeActividadTuristica] BIT,
+    [idCompraFechaCompraSk] INTEGER,
+    PRIMARY KEY ([idServicioViajeSk])
+)
+GO
+
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [DimRutaLugar_FactServicioViaje] 
+    FOREIGN KEY ([idRutaLugarSk]) REFERENCES [DimRutaLugar] ([idRutaLugarSk])
+GO
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [DimConductor_FactServicioViaje] 
+    FOREIGN KEY ([idConductorSk]) REFERENCES [DimConductor] ([idConductorSk])
+GO
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [DimAutobus_FactServicioViaje] 
+    FOREIGN KEY ([idAutobusSk]) REFERENCES [DimAutobus] ([idAutobusSk])
+GO
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [DimTurista_FactServicioViaje] 
+    FOREIGN KEY ([idTuristaSk]) REFERENCES [DimTurista] ([idTuristaSk])
+GO
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [DimBoleto_FactServicioViaje] 
+    FOREIGN KEY ([idBoletoSk]) REFERENCES [DimBoleto] ([idBoletoSk])
+GO
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [%parentxx%_FactServicioViaje] 
+    FOREIGN KEY ([idViajeFechaViajeSk]) REFERENCES [DimTiempo] ([idTiempoSk])
+GO
+
+ALTER TABLE [FactServicioViaje] ADD CONSTRAINT [DimTiempo_FactServicioViaje] 
+    FOREIGN KEY ([idCompraFechaCompraSk]) REFERENCES [DimTiempo] ([idTiempoSk])
+GO
