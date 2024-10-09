6fdbbfab43cb417934c023b9c4555fdd26393e50
Roger Diego Flores Condori <rogerdiegof@gmail.com>
2024-10-08 21:12:00 -0400

    version funcional etl

--- Data-Proy-Maestria/Scripts/Autobus.data.sql
+++ Data-Proy-Maestria/Scripts/Autobus.data.sql
@@ -1,20 +1,26 @@
-﻿INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('ABC123', 'Sprinter', 'Mercedes-Benz', 20, 'Aire acondicionado, Asientos reclinables');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('DEF456', 'Hiace', 'Toyota', 15, 'Asientos de cuero, Climatizador');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('GHI789', 'Transit', 'Ford', 18, 'Ventanas panorámicas, Wi-Fi');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('JKL012', 'Coaster', 'Toyota', 24, 'Asientos reclinables, Pantallas de entretenimiento');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('MNO345', 'Daily', 'Iveco', 30, 'Conectores USB, Luz individual');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('PQR678', 'Sirius', 'Volkswagen', 35, 'Aire acondicionado, Wi-Fi');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('STU901', 'Econoline', 'Ford', 12, 'Espacio para equipaje, Ventanas polarizadas');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('VWX234', 'Sprinter', 'Mercedes-Benz', 28, 'Climatizador, Pantallas de entretenimiento');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('YZA567', 'Hiace', 'Toyota', 22, 'Asientos reclinables, Luz individual');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('BCD890', 'Transit', 'Ford', 16, 'Wi-Fi, Asientos de cuero');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('EFG123', 'Coaster', 'Toyota', 20, 'Aire acondicionado, Pantallas de entretenimiento');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('HIJ456', 'Daily', 'Iveco', 24, 'Conectores USB, Asientos reclinables');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('KLM789', 'Sirius', 'Volkswagen', 18, 'Luz individual, Ventanas panorámicas');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('NOP012', 'Sprinter', 'Mercedes-Benz', 26, 'Aire acondicionado, Wi-Fi');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('QRS345', 'Hiace', 'Toyota', 14, 'Asientos de cuero, Espacio para equipaje');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('TUV678', 'Transit', 'Ford', 20, 'Climatizador, Luz individual');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('WXY901', 'Coaster', 'Toyota', 25, 'Pantallas de entretenimiento, Conectores USB');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('ZAB234', 'Daily', 'Iveco', 32, 'Wi-Fi, Aire acondicionado');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('CDE567', 'Sirius', 'Volkswagen', 22, 'Asientos reclinables, Luz individual');
-INSERT INTO [Autobus] ([matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES ('FGH890', 'Sprinter', 'Mercedes-Benz', 27, 'Aire acondicionado, Ventanas panorámicas');
+﻿
+SET NOCOUNT ON;
+SET IDENTITY_INSERT [Autobus] ON; 
+
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (1, 'ABC123', 'Sprinter', 'Mercedes-Benz', 20, 'Aire acondicionado, Asientos reclinables');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (2, 'DEF456', 'Hiace', 'Toyota', 15, 'Asientos de cuero, Climatizador');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (3, 'GHI789', 'Transit', 'Ford', 18, 'Ventanas panorámicas, Wi-Fi');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (4, 'JKL012', 'Coaster', 'Toyota', 24, 'Asientos reclinables, Pantallas de entretenimiento');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (5, 'MNO345', 'Daily', 'Iveco', 30, 'Conectores USB, Luz individual');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (6, 'PQR678', 'Sirius', 'Volkswagen', 35, 'Aire acondicionado, Wi-Fi');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (7, 'STU901', 'Econoline', 'Ford', 12, 'Espacio para equipaje, Ventanas polarizadas');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (8, 'VWX234', 'Sprinter', 'Mercedes-Benz', 28, 'Climatizador, Pantallas de entretenimiento');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (9, 'YZA567', 'Hiace', 'Toyota', 22, 'Asientos reclinables, Luz individual');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (10,'BCD890', 'Transit', 'Ford', 16, 'Wi-Fi, Asientos de cuero');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (11,'EFG123', 'Coaster', 'Toyota', 20, 'Aire acondicionado, Pantallas de entretenimiento');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (12,'HIJ456', 'Daily', 'Iveco', 24, 'Conectores USB, Asientos reclinables');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (13,'KLM789', 'Sirius', 'Volkswagen', 18, 'Luz individual, Ventanas panorámicas');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (14,'NOP012', 'Sprinter', 'Mercedes-Benz', 26, 'Aire acondicionado, Wi-Fi');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (15,'QRS345', 'Hiace', 'Toyota', 14, 'Asientos de cuero, Espacio para equipaje');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (16,'TUV678', 'Transit', 'Ford', 20, 'Climatizador, Luz individual');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (17,'WXY901', 'Coaster', 'Toyota', 25, 'Pantallas de entretenimiento, Conectores USB');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (18,'ZAB234', 'Daily', 'Iveco', 32, 'Wi-Fi, Aire acondicionado');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (19,'CDE567', 'Sirius', 'Volkswagen', 22, 'Asientos reclinables, Luz individual');
+INSERT INTO [Autobus] ([idAutobus],[matricula], [modelo], [fabricante], [numeroPlazas], [caracteristicas]) VALUES (20,'FGH890', 'Sprinter', 'Mercedes-Benz', 27, 'Aire acondicionado, Ventanas panorámicas');
+
+SET IDENTITY_INSERT [Autobus] OFF;
