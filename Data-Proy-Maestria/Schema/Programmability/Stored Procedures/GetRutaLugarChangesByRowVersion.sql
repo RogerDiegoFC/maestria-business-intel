25a5ca75510f8b71342c4d60cc37f26e2d3ee0d7
Esperanza Miriam Aquino Mamani <speranzamiriam2@gmail.com>
2024-10-11 13:52:29 -0400

    ajuste a base  oltp

--- Data-Proy-Maestria/Schema/Programmability/Stored Procedures/GetRutaLugarChangesByRowVersion.sql
+++ Data-Proy-Maestria/Schema/Programmability/Stored Procedures/GetRutaLugarChangesByRowVersion.sql
@@ -8,18 +8,9 @@ BEGIN
       ,r.[nombreRuta]
       ,r.[duracionAproximadaHoras]
       ,r.[esMasAtractiva]
-      ,r.[kilometrosRecorrido]
-	  ,l.[idLugar]
-      ,l.[nombreLugar]
-      ,l.[horaPrevistaLlegada]
-      ,l.[actividad]
-      ,l.[tiempoParadaHoras]
-  FROM [dbo].[Ruta] r 
-  inner join dbo.Lugar l 
-  on r.idRuta= l.idRuta
+      ,r.[kilometrosRecorrido]	 
+  FROM [dbo].[Ruta] r  
   WHERE (r.[rowversion] > CONVERT(ROWVERSION,@startRow) 
-		   AND r.[rowversion] <= CONVERT(ROWVERSION,@endRow))
-	OR (l.[rowversion] > CONVERT(ROWVERSION,@startRow) 
-		   AND l.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+		   AND r.[rowversion] <= CONVERT(ROWVERSION,@endRow))	
 END
 go
